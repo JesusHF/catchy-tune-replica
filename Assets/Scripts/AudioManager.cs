a75bac4950f180604f7f4344ee748b707df44372
Jesus Hernandez <jehernandez1996@gmail.com>
2021-01-26 23:03:30 +0100

    player animations moving with tutorial loop

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource songAudioSource;
+    public AudioSource sfxAudioSource;
+
+    public Sound[] songs;
+    public Sound[] sfxSounds;
+
+    #region Singleton
+    public static AudioManager instance { get; private set; }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    public void PlaySong(string name, bool loop = false)
+    {
+        var music = Array.Find(songs, song => song.name == name);
+        Assert.IsNotNull(music, "Song '" + name + "' not found");
+
+        songAudioSource.clip = music.clip;
+        songAudioSource.loop = loop;
+        songAudioSource.Play();
+    }
+
+    public void PlaySfx(string name)
+    {
+        var sfx = Array.Find(sfxSounds, sound => sound.name == name);
+        Assert.IsNotNull(sfx, "SFX '" + name + "' not found");
+
+        sfxAudioSource.PlayOneShot(sfx.clip);
+    }
+
+    public void StopCurrentSfx()
+    {
+        sfxAudioSource.Stop();
+    }
+
+    public void FadeCurrentSong(float duration, float targetVolume = 0f)
+    {
+        StartCoroutine(StartFade(duration, targetVolume));
+    }
+
+    private IEnumerator StartFade(float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = songAudioSource.volume;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            songAudioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+
+        songAudioSource.Stop();
+        songAudioSource.volume = 1f;
+        yield break;
+    }
+}
---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    private float songBpm;
+    private float secPerBeat;
+    public float songPosition { get; private set; }
+    public int songPositionMs { get; private set; }
+    public float songPositionInBeats { get; private set; }
+    public float dspSongTime { get; private set; }
+
+    private float beatsPerLoop;
+    public int completedLoops { get; private set; }
+    public float loopPositionInBeats { get; private set; }
+    public float loopPositionInAnalog { get; private set; }
+
+    #region Singleton
+    public static Conductor instance { get; private set; }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    #endregion
+
+    public void StartSong(/*SongInfo songInfo*/)
+    {
+        songBpm = 130;
+        secPerBeat = 60f / songBpm;
+        dspSongTime = (float)AudioSettings.dspTime;
+        beatsPerLoop = 8;
+
+        //AudioManager.Instance().PlaySong(songInfo.song_clip);
+        AudioManager.instance.PlaySong("tutorial", true);
+    }
+
+    void Update()
+    {
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+        songPositionMs = (int)(songPosition * 1000);
+        songPositionInBeats = songPosition / secPerBeat;
+
+        if (songPositionInBeats >= (completedLoops + 1) * beatsPerLoop)
+            completedLoops++;
+        loopPositionInBeats = songPositionInBeats - completedLoops * beatsPerLoop;
+        loopPositionInAnalog = loopPositionInBeats / beatsPerLoop;
+    }
+
+}

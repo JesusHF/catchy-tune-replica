16c2d78417e4557327af64614ea46583038d1e64
Jesus Hernandez <jehernandez1996@gmail.com>
2021-01-28 13:32:29 +0100

    created tutorialManager

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI numberText;
+    [SerializeField]
+    private FruitSpawner fruitSpawner;
+    private int itemsLeft;
+    private int nextBeatToSpawnItem;
+    private TutorialStates tutorialState;
+
+    // tutorial states
+    private enum TutorialStates
+    {
+        SpawnLoop,
+        End
+    }
+
+    void Start()
+    {
+        tutorialState = TutorialStates.SpawnLoop;
+        itemsLeft = 3;
+        numberText.text = itemsLeft.ToString();
+        nextBeatToSpawnItem = 12;
+    }
+
+    void Update()
+    {
+        switch (tutorialState)
+        {
+            case TutorialStates.SpawnLoop:
+                SpawnOrangeLoop();
+                break;
+            case TutorialStates.End:
+                // end tutorial, start game
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SpawnOrangeLoop()
+    {
+        float currentBeat = Conductor.instance.songPositionInBeats;
+        if (currentBeat >= nextBeatToSpawnItem)
+        {
+            nextBeatToSpawnItem += 16;
+            fruitSpawner.SpawnOrange();
+        }
+    }
+
+    void DecreaseNumber()
+    {
+        if (itemsLeft == 1)
+        {
+            tutorialState++;
+        }
+        else
+        {
+            itemsLeft--;
+            numberText.text = itemsLeft.ToString();
+        }
+    }
+
+}

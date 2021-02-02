eed126baba038f52f1a7339ca786a1870d9f728e
Jesus Hernandez <jehernandez1996@gmail.com>
2021-02-02 18:39:10 +0100

    added to tutorialManager spawnPineAppleLoop

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -12,13 +12,15 @@ public class TutorialManager : MonoBehaviour
     [SerializeField]
     private int nextBeatToSpawnItem = 11;
     private int itemsLeft;
+    private Instrument nextInstrument;
     private TutorialStates tutorialState;
 
     private enum TutorialStates
     {
         None,
-        SpawnLoopRight,
-        SpawnLoopLeft,
+        SpawnLoopOrangesRight,
+        SpawnLoopOrangesLeft,
+        SpawnLoopPineApples,
         End
     }
 
@@ -52,11 +54,14 @@ public class TutorialManager : MonoBehaviour
         }
         switch (tutorialState)
         {
-            case TutorialStates.SpawnLoopRight:
+            case TutorialStates.SpawnLoopOrangesRight:
                 SpawnInstrumentLoop(Instrument.orangeR);
                 break;
-            case TutorialStates.SpawnLoopLeft:
+            case TutorialStates.SpawnLoopOrangesLeft:
                 SpawnInstrumentLoop(Instrument.orangeL);
+                break;
+            case TutorialStates.SpawnLoopPineApples:
+                SpawnPineApplesLoop();
                 break;
             case TutorialStates.End:
                 // component should be disabled
@@ -74,21 +79,37 @@ public class TutorialManager : MonoBehaviour
             GameManager.instance.CreateKeynoteNow(instrument);
         }
     }
+    private void SpawnPineApplesLoop()
+    {
+        if (Conductor.instance.songPositionInBeats >= nextBeatToSpawnItem)
+        {
+            if(nextInstrument == Instrument.pineAppleR)
+            {
+                nextInstrument = Instrument.pineAppleL;
+            }
+            else
+            {
+                nextInstrument = Instrument.pineAppleR;
+            }
+            nextBeatToSpawnItem += 8;
+            GameManager.instance.CreateKeynoteNow(nextInstrument);
+        }
+    }
 
     private void GetNextState()
     {
         tutorialState++;
         switch (tutorialState)
         {
-            case TutorialStates.SpawnLoopRight:
-                itemsLeft = 3;
-                numberText.text = itemsLeft.ToString();
-                moreTimesText.text = "More times!";
+            case TutorialStates.SpawnLoopOrangesRight:
+                ResetUI();
                 break;
-            case TutorialStates.SpawnLoopLeft:
-                itemsLeft = 3;
-                numberText.text = itemsLeft.ToString();
-                moreTimesText.text = "More times!";
+            case TutorialStates.SpawnLoopOrangesLeft:
+                ResetUI();
+                break;
+            case TutorialStates.SpawnLoopPineApples:
+                ResetUI();
+                nextBeatToSpawnItem += nextBeatToSpawnItem % 8;
                 break;
             case TutorialStates.End:
                 EndTutorial();
@@ -126,6 +147,13 @@ public class TutorialManager : MonoBehaviour
         moreTimesText.gameObject.SetActive(show);
         numberText.gameObject.SetActive(show);
         pressEscapeText.gameObject.SetActive(show);
+    }
+
+    private void ResetUI()
+    {
+        itemsLeft = 3;
+        numberText.text = itemsLeft.ToString();
+        moreTimesText.text = "More times!";
     }
 
 }

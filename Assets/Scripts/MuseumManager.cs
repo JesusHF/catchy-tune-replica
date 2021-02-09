e3575502ee86a56a9bddc7db789f54c3793e33cd
Jesus Hernandez <jehernandez1996@gmail.com>
2021-02-09 11:01:16 +0100

    created levelSelection scene and museumManager script

---
+++ Assets/Scripts/MuseumManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuseumManager : MonoBehaviour
+{
+    public Image blackSquareImage;
+
+    void Start()
+    {
+        StartCoroutine(FadeBlackSquare(2f, 0f, () =>
+        {
+            AudioManager.instance.PlaySong("museum", true);
+        }));
+    }
+
+    void Update()
+    {
+
+    }
+
+    private IEnumerator FadeBlackSquare(float duration, float finalAlpha = 0f, Action onFinished = null)
+    {
+        float currentTime = 0;
+        float startAlpha = 1 - finalAlpha;
+        Color c = blackSquareImage.color;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            float a = Mathf.Lerp(startAlpha, finalAlpha, currentTime / duration);
+            blackSquareImage.color = new Color(c.r, c.g, c.b, a);
+            yield return null;
+        }
+
+        onFinished?.Invoke();
+        yield break;
+    }
+}
